a517814392c812d9fe0655774556f929980f85c0
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2020-11-19 09:43:02 +0700

    start

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python Main/manage.py makemigrations && \
-python Main/manage.py migrate && \
-python Main/manage.py runserver 0.0.0.0:8000
+python Main/manage.py makemigrations && python Main/manage.py migrate && python Main/manage.py runserver 0.0.0.0:8000
